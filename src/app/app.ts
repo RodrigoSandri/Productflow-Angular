5001cb231f45d74bd7105507b594e14ec318368f
Rodrigo Sandri <102749822+RodrigoSandri@users.noreply.github.com>
2026-04-29 13:55:01 -0300

    primeiro commit do projeto com Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('aula-angular');
+   nome = 'Lucas Fogaça'
 }
